db8297a1149adf8cb68b4a50dde83f30a178dd28
Nilugette <nilugette@yahoo.fr>
2020-12-11 11:03:17 +0100

    Modification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,15 +12,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://photo-app-by-laure.herokuapp.com/', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
